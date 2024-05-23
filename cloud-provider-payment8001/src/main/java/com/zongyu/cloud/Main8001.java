7814302393ef5bcd2c6d310e20c6a2afc38ab709
yuzong921115 <yuzong00000@gmail.com>
2024-05-23 15:57:16 +0800

    服务提供者注册到consul

--- cloud-provider-payment8001/src/main/java/com/zongyu/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/zongyu/cloud/Main8001.java
@@ -3,6 +3,7 @@ package com.zongyu.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -10,6 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan("com.zongyu.cloud.mapper") // import tk.mybatis.spring.annotation.MapperScan
+@EnableDiscoveryClient // 开启服务注册与发现
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
