d52a1c5cf9a2c1a7287dd44c8b8e72e73cca703d
yuzong921115 <yuzong00000@gmail.com>
2024-05-28 16:15:18 +0800

    舱壁，线程池

--- cloud-feign-consumer-order80/src/main/java/com/zongyu/cloud/controller/OrderCircuitController.java
+++ cloud-feign-consumer-order80/src/main/java/com/zongyu/cloud/controller/OrderCircuitController.java
@@ -8,11 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class OrderCircuitController {
     @Resource
     private PayFeignClient payFeignClient;
 
+    /**
+     * 熔断+降级示例
+     *
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/feign/pay/circuit/{id}")
     @CircuitBreaker(name = "cloud-payment-service", fallbackMethod = "myCircuitFallback")
     public String myCircuit(@PathVariable("id") Integer id) {
@@ -23,6 +32,12 @@ public class OrderCircuitController {
         return "myCircuitFallback , 系统服务繁忙，亲稍后再试";
     }
 
+    /**
+     * 舱壁 信号量
+     *
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/feign/pay/bulkhead/{id}")
     @Bulkhead(name = "cloud-payment-service", fallbackMethod = "myCircuitFallback2", type = Bulkhead.Type.SEMAPHORE)
     public String myCircuit2(@PathVariable("id") Integer id) {
@@ -31,5 +46,28 @@ public class OrderCircuitController {
 
     public String myCircuitFallback2(Throwable t) {
         return "myCircuitFallback bulkhead, 系统服务繁忙，亲稍后再试";
+    }
+
+    /**
+     * 舱壁 线程池
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/feign/pay/bulkhead/pool/{id}")
+    @Bulkhead(name = "cloud-payment-service", fallbackMethod = "myCircuitFallbackPool", type = Bulkhead.Type.THREADPOOL)
+    public CompletableFuture<String> bulkheadPool(@PathVariable("id") Integer id) {
+        System.out.println(Thread.currentThread().getName() + "\t" + " come in...");
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t" + " leave...");
+        return CompletableFuture.supplyAsync(() -> payFeignClient.myCircuit2(id) + "\t" + "Bulkhead.Type.THREADPOOL");
+    }
+
+    public CompletableFuture<String> myCircuitFallbackPool(Integer id, Throwable t) {
+        return CompletableFuture.supplyAsync(() -> "myCircuitFallback bulkhead pool, 系统服务繁忙，亲稍后再试");
     }
 }
