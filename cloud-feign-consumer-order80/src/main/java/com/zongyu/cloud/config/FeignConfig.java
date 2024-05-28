4ec3c1fd738a34b211136e97e0f2cc978014f486
yuzong921115 <yuzong00000@gmail.com>
2024-05-28 15:27:53 +0800

    silience4j时间熔断

--- cloud-feign-consumer-order80/src/main/java/com/zongyu/cloud/config/FeignConfig.java
+++ cloud-feign-consumer-order80/src/main/java/com/zongyu/cloud/config/FeignConfig.java
@@ -10,10 +10,10 @@ public class FeignConfig {
     @Bean
     public Retryer myRetryer() {
         // Feign默认配置不走重试策略
-//        return Retryer.NEVER_RETRY;
+        return Retryer.NEVER_RETRY;
 
         // 最大请求次数为3（1+2），初始间隔时间为100ms，重试最大间隔时间为1s
-        return new Retryer.Default(100, 1, 3);
+//        return new Retryer.Default(100, 1, 3);
     }
 
     @Bean
