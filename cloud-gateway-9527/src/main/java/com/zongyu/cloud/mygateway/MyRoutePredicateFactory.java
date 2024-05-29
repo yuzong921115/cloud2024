838a132c7536b25a0d2d0a56027e77cb1849b8e4
yuzong921115 <yuzong00000@gmail.com>
2024-05-29 16:10:15 +0800

    gatewa 内置过滤器

--- cloud-gateway-9527/src/main/java/com/zongyu/cloud/mygateway/MyRoutePredicateFactory.java
+++ cloud-gateway-9527/src/main/java/com/zongyu/cloud/mygateway/MyRoutePredicateFactory.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 /**
- * 需求说明：自定义会员等级
+ * 需求说明：自定义会员等级 参考org.springframework.cloud.gateway.handler.predicate.AfterRoutePredicateFactory
  */
 @Component
 public class MyRoutePredicateFactory extends AbstractRoutePredicateFactory<MyRoutePredicateFactory.Config> {
--- cloud-provider-payment8001/src/main/java/com/zongyu/cloud/controller/PayGateWayController.java
+++ cloud-provider-payment8001/src/main/java/com/zongyu/cloud/controller/PayGateWayController.java
@@ -1,5 +1,6 @@
 package com.zongyu.cloud.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.zongyu.cloud.model.Pay;
 import com.zongyu.cloud.resp.ResultData;
@@ -7,10 +8,13 @@ import com.zongyu.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
 
 @RestController
 @Slf4j
@@ -29,5 +33,28 @@ public class PayGateWayController {
     @GetMapping(value = "/pay/gateway/info")
     public ResultData<String> infoByGateWay() {
         return ResultData.success("gateway info test:" + IdUtil.simpleUUID());
+    }
+
+    /**
+     * filter 请求头
+     */
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request) {
+        String customerId = request.getParameter("customerId");
+        String customerName = request.getParameter("customerName");
+        System.out.println("customerId=" + customerId + "\tcustomerName=" + customerName);
+
+        String result = "";
+        Enumeration<String> headers = request.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String headName = headers.nextElement();
+            String headValue = request.getHeader(headName);
+            System.out.println("请求头：" + headName + "\t请求值：" + headValue);
+            if (headName.equalsIgnoreCase("X-Request-zongyu1")
+                    || headName.equalsIgnoreCase("X-Request-zongyu2")) {
+                result = result + headName + "\t" + headValue + " ";
+            }
+        }
+        return ResultData.success("getGatewayFilter 过滤器Test：" + result + "\t" + DateUtil.now());
     }
 }
