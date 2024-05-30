e55ff0f26d89caac58ca94168b065627c5377fb4
yuzong921115 <yuzong00000@gmail.com>
2024-05-30 20:31:02 +0800

    sentinel 流控效果和规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/zongyu/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zongyu/cloud/controller/FlowLimitController.java
@@ -1,10 +1,15 @@
 package com.zongyu.cloud.controller;
 
+import com.zongyu.cloud.service.FlowLimitService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FlowLimitController {
+    @Resource
+    private FlowLimitService flowLimitService;
+
     @GetMapping("/testA")
     public String testA() {
         return "super.toString() AA";
@@ -13,5 +18,30 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "super.toString() BBBB";
+    }
+
+    /**
+     * 测试链路限流 C受限 D正常
+     *
+     * @return
+     */
+    @GetMapping("/testC")
+    public String testC() {
+        flowLimitService.common();
+        return "super.toString() C";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        flowLimitService.common();
+        return "super.toString() DDDDDD";
+    }
+
+    /**
+     * 流控 排队等待
+     */
+    @GetMapping("/testE")
+    public void testE() {
+        System.out.println("排队等待testE");
     }
 }
