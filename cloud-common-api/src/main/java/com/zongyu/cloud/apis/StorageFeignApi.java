635678a5e3f01f18ff172d11fac701fbd03bb1a9
yuzong921115 <yuzong00000@gmail.com>
2024-05-31 18:09:11 +0800

    feign

---
+++ cloud-common-api/src/main/java/com/zongyu/cloud/apis/StorageFeignApi.java
@@ -0,0 +1,19 @@
+package com.zongyu.cloud.apis;
+
+import com.zongyu.cloud.resp.ResultData;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "seata-storage-service")
+public interface StorageFeignApi {
+    /**
+     * 扣减库存
+     *
+     * @param productId
+     * @param count
+     * @return
+     */
+    @PostMapping(value = "/storage/decrease")
+    ResultData decrease(@RequestParam("productId") Long productId, @RequestParam("userId") Integer count);
+}
