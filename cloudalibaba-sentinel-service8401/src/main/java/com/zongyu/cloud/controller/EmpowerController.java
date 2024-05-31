89d9c1520d0467b2e837736d2144d6209a6438b1
yuzong921115 <yuzong00000@gmail.com>
2024-05-31 10:47:54 +0800

    热点规则

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zongyu/cloud/controller/EmpowerController.java
@@ -0,0 +1,15 @@
+package com.zongyu.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class EmpowerController {
+    @GetMapping(value = "/empower")
+    public String requestSentinel4() {
+        log.info("测试Sentinel授权规则empower");
+        return "Sentinel授权规则";
+    }
+}
--- cloudalibaba-sentinel-service8401/src/main/java/com/zongyu/cloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/zongyu/cloud/controller/RateLimitController.java
@@ -4,6 +4,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +39,23 @@ public class RateLimitController {
 
     public String doActionFallBack(@PathVariable("id") Integer id, Throwable throwable) {
         return "JVM抛出异常";
+    }
+
+    /**
+     * 热点参数限流
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    @GetMapping("/testHotKeys")
+    @SentinelResource(value = "testHotKeys", blockHandler = "dealHandler_testHotKeys")
+    public String testHotKeys(@RequestParam(value = "p1", required = false) String p1
+            , @RequestParam(value = "p2", required = false) String p2) {
+        return "come in 热点参数限流";
+    }
+
+    public String dealHandler_testHotKeys(String p1, String p2, BlockException exception) {
+        return "触发热点参数限流";
     }
 }
